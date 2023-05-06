560aaaf14965289786123085d834ec80a02ba2a1
Grammyship <ss96083@gmail.com>
2023-05-06 21:40:42 +0800

    Part3後端更新

--- Loki/intent/Loki_Part_3.py
+++ Loki/intent/Loki_Part_3.py
@@ -49,479 +49,207 @@ def getResponse(utterance, args):
 def getResult(inputSTR, utterance, args, resultDICT):
     debugInfo(inputSTR, utterance)
     if utterance == "一天解決痘疤":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "一次解決":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "任何乳房立即增大":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "促進內分泌與造血功能":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "促進分解皮下脂肪":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "促進細胞代謝":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "促進細胞氧化":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "促進肌膚再生":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "促進體內乳腺細胞活化":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "保養關節":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "修補傷口":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "健全免疫機能":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "傷口癒合能力":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "傷痕修復":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "全部都消失":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "利尿":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "刺激乳腺發育":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "刺激胸部脂肪組織":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "加強肌膚表層細胞再生之機能":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "加強表皮細胞的再生能力":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "增強免疫力":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "安撫躁進或心情不穩的脾氣":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "對於呼吸、聽覺系統有助益":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "強化夫妻情感聯繫":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "抑制巨大細胞的過敏":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "抑制血小板的凝集":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "抑制體毛生長":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "排出體內多餘毒素":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "排出體內多餘水分":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "排除皮下脂肪":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "提升肌膚含氧量":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "提振精神":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "提神醒腦":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "攔截神經傳遞":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "改善內分泌代謝":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "改善內部體質":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "改善過敏皮膚的體質":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "殺菌":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "消浮腫":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "消除已形成之黑斑":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "消除皮屑芽孢菌":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "淨身":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "清除水分及脂肪屯積":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "減少受傷細胞":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "減少蚊蟲叮咬":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "潤滑":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "瓦解脂肪":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "直接到達乳房組織":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "禁止咬甲":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "精力充沛":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "經皮吸收血管穿透素":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "維持陰道環境 PH 值正常":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "維持陰道酸性環境":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "緩解關節與肌肉疼痛":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "舒緩喉嚨痛":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "舒解其痛苦":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "費洛蒙":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "進行脂肪的分解":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "避邪":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "重建肌膚":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "關節靈活":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "防疫產品":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "降低懷孕生理不適":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "隔離PM2.5":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "隔離空氣汙染":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "集中注意力":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "預防病菌入侵":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     if utterance == "預防腸病毒":
-        if CHATBOT_MODE:
-            resultDICT["response"] = getResponse(utterance, args)
-        else:
-            # write your code here
-            pass
+        resultDICT["Part3"].append(inputSTR)
 
     return resultDICT
